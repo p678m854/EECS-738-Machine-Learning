4c22eafa4ffd529c2cbe035ab4a75970e10246e7
Patrick McNamee <p678m854@gmail.com>
2019-02-12 11:05:52 -0600

    Create mdvar.py
    Multidimensional variance, essentially the variance of norms of data from the mean

---
+++ mdvar.py
@@ -0,0 +1,20 @@
+"""
+Multidimensional Variance: Variance of norms
+
+Inputs:
+	data == list of data around mean
+	mean == mean of data
+	
+Output:
+	RSS == residuals sum of squares
+"""
+# necessary library for norm
+import numpy as np
+
+def mdvar(data, mean):
+	RSS = 0
+	N = data.shape[0] #Number of data points
+	
+	for i in range(N):
+		
+	return RSS
